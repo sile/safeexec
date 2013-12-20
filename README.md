0f2e5afbd245d7024d9d7262b26d4a2e81d8c5c2
Takeru Ohta <phjgt308@gmail.com>
2013-12-20 11:40:06 -0800

    implements safeexec command

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ safeexecコマンド
 safeexe 経由で実行された外部コマンドは、以下の性質を備えるようになる:
 
 - ErlangVM(プロセス)終了時に、確実に終了する
-- erlang:port_close/1 あるいは リンクプロセスダウン により、外部コマンドに紐づくポートが終了した場合に、確実に終了する
+- `erlang:port_close/1`あるいは`リンクプロセスのダウン`により、外部コマンドに紐づくポートが終了した場合に、確実に終了する
 
 つまり、外部コマンドを safeexec 経由で呼び出すことにより、  
 利用者は外部コマンドポートを、Erlangの通常のプロセスに近い終了モデル(リンクしているプロセスが終了した場合は自分も終了する)で扱えるようになる。
@@ -29,4 +29,4 @@ Erlangインタフェース
 
 - 第一引数が`{spawn, Command}`の場合は`Command`の前にsafeexecコマンドのパスが付与される
 - 第一引数が`{spawn_executable, FileName}`の場合は`FileName`の値はsafeexecコマンドのパスに置換され、もともとの`FileName`の値は`args`オプションの先頭に挿入される
-- `{spawn_driver, _}`および`{fd, In, Out}`は非サポート
+- `{spawn_driver, Command}`および`{fd, In, Out}`は非サポート
--- c_src/safeexec.c
+++ c_src/safeexec.c
@@ -3,82 +3,91 @@
  */
 
 #include <stdio.h>
-#include <unistd.h>
-#include <errno.h>
+#include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+#include <unistd.h>
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/prctl.h>
 
-int main(int argc, char ** argv) {
+#define ERRMSG(Message) fprintf(stderr, "[error:%d] " Message ": pid=%d, ppid=%d, error=%s(%d)\n", __LINE__, getpid(), getppid(), strerror(errno), errno)
+#define ERR_EXIT(Message) {ERRMSG(Message); exit(1);}
+
+int parent_main(pid_t child_pid);
+
+int main(int argc, char ** argv)
+{
   if (argc < 2) {
-    fprintf(stderr, "Usage: safeexec COMMAND COMMAND_ARGS...\n");
+    fprintf(stderr, "Usage: safeexec COMMAND [COMMAND_ARG...]\n");
     return 1;
+  }
+  {
+    const char * command_path = argv[1];
+    char ** command_args = &argv[1];
+    int ret;
+
+    ret = fork();
+    if(ret == -1) { ERR_EXIT("fork() failed"); }
+
+    if(ret > 0) {
+      // parent
+      if (prctl(PR_SET_PDEATHSIG, SIGTERM) == -1) { ERR_EXIT("parent prctl() failed"); }
+      return parent_main(ret);
+    } else {
+      // child
+      if (prctl(PR_SET_PDEATHSIG, SIGKILL)   == -1) { ERR_EXIT("child prctl() failed"); }
+      if (execvp(command_path, command_args) == -1) { ERR_EXIT("execvp() faied"); }
+    }
   }
   return 0;
 }
 
-/*
-int main(int argc, char ** argv) {
+int parent_main(pid_t child_pid)
+{
+  sigset_t sigs;
+  siginfo_t info;
+  struct timespec poll_timeout = {0, 50 * 1000 * 1000};  // 50 milliseconds
   int ret;
-  const char * child_path = argv[1];
-  char ** child_args = &argv[1];
 
-  ret = fork();
-  if(ret == -1) {
-    std::cerr << "[error] fork() failed: " << strerror(errno) << "(" << errno << ")" << std::endl;
-    return 1;
-  } else if(ret > 0) {
-    prctl( PR_SET_PDEATHSIG, SIGTERM );
-
-    timespec timeout = {1, 0};
-
-    sigset_t sigs;
-    sigfillset(&sigs);
-
-    sigprocmask(SIG_SETMASK, &sigs, NULL);
-    siginfo_t siginfo;
-    // if(sigwaitinfo(&sigs, &siginfo) == -1) {
-    //     std::cerr << "[error] sigwaitinfo() failed: " << strerror(errno) << "(" << errno << ")" << std::endl;
-    //     return -1;
-    // }
-    while(sigtimedwait(&sigs, &siginfo, &timeout) == -1 && errno == EAGAIN) {
-       // std::string n;
-       // std::cin >> n;
-       // std::cerr << "read: " << n << "(" << n.size() << ")" << std::endl;
-      std::cin.peek();
-       std::cerr << "check" << std::endl;
-      if(std::cin.eof()) {
-        std::cerr << "stdin is closed!" << std::endl;
-        break;
-      }
-      if(std::cerr.eof()) {
-        std::cerr << "stdout is closed!" << std::endl;
-        break;
-      }
-    }
-    std::cerr << "[signal] " << siginfo.si_signo << std::endl;
-    
-    if(siginfo.si_signo != SIGCHLD) {
-      kill(ret, SIGTERM);
-
-      sigtimedwait(&sigs, &siginfo, &timeout);
-      if(siginfo.si_signo !=SIGCHLD) {
-        kill(ret, SIGKILL);
-      }
-    }
-    int status;
-    waitpid(ret, &status, 0);
-    std::cerr << "[finish] " << status << std::endl;
-    return status;
-  } else {
-    prctl( PR_SET_PDEATHSIG, SIGKILL );
-    if(execvp(child_path, child_args) == -1) {
-      std::cerr << "[error] execvp() failed: " << errno << std::endl;
-      return 1;
+  if (sigfillset(&sigs)                     == -1) { ERRMSG("sigfillset() failed");  goto kill_child; }
+  if (sigprocmask(SIG_SETMASK, &sigs, NULL) == -1) { ERRMSG("sigprocmask() failed"); goto kill_child; }
+  
+  while ((ret = sigtimedwait(&sigs, &info, &poll_timeout)) == -1) {
+    if (errno != EAGAIN || feof(stdin)) {
+      break;
     }
   }
-  return 0;
+
+  if (ret == -1 || info.si_signo != SIGCHLD) {
+    struct timespec wait_timeout = {1, 0}; // 1 second
+
+    if (kill(child_pid, SIGTERM) == -1) { ERRMSG("kill() failed"); goto kill_child; }
+
+    if (sigemptyset(&sigs) == -1)        { ERRMSG("sigemptyset() failed"); goto kill_child; }
+    if (sigaddset(&sigs, SIGCHLD) == -1) { ERRMSG("sigaddset() failed");   goto kill_child; }
+
+    if (sigtimedwait(&sigs, NULL, &wait_timeout) == -1) {
+      if(errno != EAGAIN) { ERRMSG("sigtimedwait() failed"); }
+      goto kill_child;
+    }
+  }
+  goto wait_child_exit;
+  
+ kill_child:
+  if (kill(child_pid, SIGKILL) == -1) { ERR_EXIT("kill() failed"); }
+  
+ wait_child_exit:
+  {
+    int status;
+    if (waitpid(child_pid, &status, 0) == -1) { ERR_EXIT("waitpid() failed"); }
+    if (WIFEXITED(status)) {
+      return WEXITSTATUS(status);
+    }
+    if (WIFSIGNALED(status)) {
+      return -WTERMSIG(status);
+    }
+    return 1;
+  }
 }
-*/
