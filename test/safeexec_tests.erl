553cf0fa3810a78c3c3e6134168d1725166bf279
Takeru Ohta <phjgt308@gmail.com>
2018-08-30 20:57:01 +0900

    Extend eunit timeout

--- test/safeexec_tests.erl
+++ test/safeexec_tests.erl
@@ -12,18 +12,19 @@
 open_port_test_() ->
     [
      {"unix kill(SIGKILL)",
-      fun () ->
-              lists:foreach(fun (I) ->
-                                    Port = safeexec:open_port({spawn_executable, "/bin/sleep"}, [{args, ["100"]}, exit_status]),
-                                    OsPid = proplists:get_value(os_pid, erlang:port_info(Port)),
-                                    os:cmd("kill -9 " ++ integer_to_list(OsPid)),
-                                    receive
-                                        {Port, {exit_status, _}} -> ?assert(true)
-                                    after 500 ->
-                                            ?debugVal(I),
-                                            ?debugVal(erlang:port_info(Port)),
-                                            ?assert(false)
-                                    end
-                            end, lists:seq(1, 3000))
-      end}
+      {timeout, 60,
+       fun () ->
+               lists:foreach(fun (I) ->
+                                     Port = safeexec:open_port({spawn_executable, "/bin/sleep"}, [{args, ["100"]}, exit_status]),
+                                     OsPid = proplists:get_value(os_pid, erlang:port_info(Port)),
+                                     os:cmd("kill -9 " ++ integer_to_list(OsPid)),
+                                     receive
+                                         {Port, {exit_status, _}} -> ?assert(true)
+                                     after 500 ->
+                                             ?debugVal(I),
+                                             ?debugVal(erlang:port_info(Port)),
+                                             ?assert(false)
+                                     end
+                             end, lists:seq(1, 3000))
+       end}}
     ].
